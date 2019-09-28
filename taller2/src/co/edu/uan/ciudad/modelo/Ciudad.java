d7cdb04c69ffdd99a711d42e2aeeeaae67875436
ESTUDIANTES <ESTUDIANTES@10.3.2.252>
2019-09-27 19:54:31 -0500

    Gears 5

---
+++ taller2/src/co/edu/uan/ciudad/modelo/Ciudad.java
@@ -0,0 +1,32 @@
+package co.edu.uan.ciudad.modelo;
+
+public class Ciudad {
+
+	private String id;
+	private String departamento;
+	private String nombre;
+	
+	public String getCIUDAD_ID() {
+		return id;
+	}
+	public void setCIUDAD_ID(String cIUDAD_ID) {
+		id = cIUDAD_ID;
+	}
+	public String getDEPARTAMENTO_ID() {
+		return departamento;
+	}
+	public void setDEPARTAMENTO_ID(String dEPARTAMENTO_ID) {
+		departamento = dEPARTAMENTO_ID;
+	}
+	public String getCIUDAD_NOMBRE() {
+		return nombre;
+	}
+	public void setCIUDAD_NOMBRE(String cIUDAD_NOMBRE) {
+		nombre = cIUDAD_NOMBRE;
+	}
+	
+	
+	
+	
+	
+}
---
+++ taller2/src/co/edu/uan/usuarios/modelo/Usuario.java
@@ -0,0 +1,66 @@
+package co.edu.uan.usuarios.modelo;
+
+public class Usuario {
+
+	private String id;
+	private String nombre;
+	private String apellido;
+	private int telefono;
+	private String fechana;
+	private String sexo;
+	private String estadocivil;
+	private String ciudadresidencia;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getFechana() {
+		return fechana;
+	}
+	public void setFechana(String fechana) {
+		this.fechana = fechana;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEstadocivil() {
+		return estadocivil;
+	}
+	public void setEstadocivil(String estadocivil) {
+		this.estadocivil = estadocivil;
+	}
+	public String getCiudadresidencia() {
+		return ciudadresidencia;
+	}
+	public void setCiudadresidencia(String ciudadresidencia) {
+		this.ciudadresidencia = ciudadresidencia;
+	}
+	
+	
+	
+	
+}
